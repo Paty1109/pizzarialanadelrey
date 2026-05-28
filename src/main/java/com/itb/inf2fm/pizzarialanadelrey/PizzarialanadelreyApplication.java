ea6ee24dce4d4bbf4d606602629f0e92370e167b
Paty1109 <rm99665@estudante.edu.br>
2026-05-28 07:36:01 -0300

    criado ambiente dev

---
+++ src/main/java/com/itb/inf2fm/pizzarialanadelrey/PizzarialanadelreyApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzarialanadelrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarialanadelreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzarialanadelreyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2fm/pizzarialanadelrey/PizzarialanadelreyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzarialanadelrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzarialanadelreyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
