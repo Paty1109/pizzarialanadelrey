f4e62eb1d6adada75ef5d7841ba6b5e7cec21bfc
Paty1109 <rm99665@estudante.fieb.edu.br>
2026-06-18 15:30:41 +0000

    Camada model services em construção

---
+++ src/main/java/com/itb/inf2fm/pizzarialanadelrey/model/services/ProdutoServices.java
@@ -0,0 +1,9 @@
+package com.itb.inf2fm.pizzarialanadelrey.model.services;
+
+public class ProdutoServices {
+
+    // Lista temporária de produtos 
+
+    private static List<Produto> PRODUTOS = new ArrayList<>();
+
+}
